805d5e3f02498939f8e54c9d433820fdae692649
ANS <36783010+answebdev@users.noreply.github.com>
2021-09-06 13:45:32 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Video: https://www.youtube.com/watch?v=F2JCjVSZlG0&t=46s
+// Code: https://github.com/weibenfalk/react-quiz
+// Live Demo: https://tender-mcnulty-a4a646.netlify.app/
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>Quiz App</p>
       </header>
     </div>
   );

4fef6d7de1c88f479304467a1307423154428f11
ANS <36783010+answebdev@users.noreply.github.com>
2021-09-07 09:56:15 -0700

    add ternaries for buttons and QuestionCard

--- src/App.tsx
+++ src/App.tsx
@@ -61,19 +61,29 @@ const App = () => {
         </button>
       ) : null}
 
-      <p className='score'>Score:</p>
-      <p>Loading Questions...</p>
-      {/* <QuestionCard
-        questionNr={number + 1}
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnswer={userAnswers ? userAnswers[number] : undefined}
-        callback={checkAnswer}
-      /> */}
-      <button className='next' onClick={nextQuestion}>
-        Next Question
-      </button>
+      {!gameOver ? <p className='score'>Score:</p> : null}
+
+      {loading ? <p>Loading Questions...</p> : null}
+
+      {!loading && !gameOver && (
+        <QuestionCard
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
+
+      {!gameOver &&
+      !loading &&
+      userAnswers.length === number + 1 &&
+      number !== TOTAL_QUESTIONS - 1 ? (
+        <button className='next' onClick={nextQuestion}>
+          Next Question
+        </button>
+      ) : null}
     </div>
   );
 };

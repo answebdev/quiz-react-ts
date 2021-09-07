c6f6a013f0a21945e5c778ba1257f71cf09b0c9a
ANS <36783010+answebdev@users.noreply.github.com>
2021-09-06 17:52:40 -0700

    add image

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 // Video: https://www.youtube.com/watch?v=F2JCjVSZlG0&t=46s
 // Code: https://github.com/weibenfalk/react-quiz
 // Live Demo: https://tender-mcnulty-a4a646.netlify.app/
+// API: https://opentdb.com/api_config.php
+// Image: https://www.pexels.com/photo/hot-air-ballons-in-the-sky-2325446/
 
 function App() {
   return <div className='App'>Quiz</div>;

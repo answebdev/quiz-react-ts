0f96f3c8bc782ce37ee1e26c4cd0871fdd0d3b7e
ANS <36783010+answebdev@users.noreply.github.com>
2021-09-06 17:41:57 -0700

    add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 // Video: https://www.youtube.com/watch?v=F2JCjVSZlG0&t=46s
 // Code: https://github.com/weibenfalk/react-quiz
 // Live Demo: https://tender-mcnulty-a4a646.netlify.app/
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Quiz App</p>
-      </header>
-    </div>
-  );
+  return <div className='App'>Quiz</div>;
 }
 
 export default App;
